6a9aa8d784b9f67da485e70b9346123c84dc9f9d
Mohammad Shanoor <107303734+shanmd975@users.noreply.github.com>
2023-02-10 22:20:43 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const ChildComponent = () => {
+const ChildComponent = (props) => {
 //code here
   return (
     <div id="child">
       Find The Square <br/>
-     
+     {props.children}
     </div>
   )
 }
-
 
 export default ChildComponent;
